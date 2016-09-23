fd6ad3aa309faacdbe9c595c43315d44fab44409
Bang Nguyen <bangnv77@gmail.com>
2016-09-23 11:33:23 -0700

    modify routes and add navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users
+
+  resources :users do
+    resources :posts
+  end
+
+  resources :users do
+    resources :comments
+  end
+
   root to: "application#index"
 end
