aebab9ce05d6f3c3fa78787e9a59b6b990420dec
Bang Nguyen <bangnv77@gmail.com>
2016-09-23 10:37:20 -0700

    create User model through Devise and set up routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+    user_path(resource)
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit :username, :email, :password, :password_confirmation
+    end
+  end
+
+  def index
+  end
 end
