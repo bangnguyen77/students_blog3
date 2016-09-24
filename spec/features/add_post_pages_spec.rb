1f9df4e3fd0b1e4d8187bc1ada651277dda211f3
Bang Nguyen <bangnv77@gmail.com>
2016-09-23 17:30:53 -0700

    pass the test for add_post_pages_spec

--- spec/features/add_post_pages_spec.rb
+++ spec/features/add_post_pages_spec.rb
@@ -2,17 +2,15 @@ require 'rails_helper'
 
 describe "the add a post process" do
   it "adds a new post" do
-    user = User.create(user)
+    user = FactoryGirl.create(:user)
     visit new_user_session_path
     fill_in 'Email', :with => 'abc@gmail.com'
     fill_in 'Password', :with => 'password'
     click_button 'Log in'
-    click_button 'Add Post'
-    fill_in "new_user_post_link", :with => "First post"
+    click_on 'Add Post'
+    fill_in "Title", :with => "First post"
     fill_in "Content", :with => "Blah aha"
     click_on "Create Post"
     expect(page).to have_content "First post"
   end
-
-
 end
